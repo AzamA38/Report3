873981b82a7722468c3fd03d306b3d8ecc524ff8
e185731 <e185731@ie.u-ryukyu.ac.jp>
2019-02-08 21:51:16 +0900

    step1 単位欲しい。

--- src/test/jp/ac/uryukyu/ie/e185731/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e185731/EnemyTest.java
@@ -4,9 +4,17 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class EnemyTest {
+public class EnemyTest {
 
     @Test
-    void attack() {
+    public void attack() {
+        int heroHP = 10;
+        Hero hero = new Hero("テスト勇者", heroHP, 5);
+        Enemy enemy = new Enemy("テストスライム", 6, 3);
+        enemy.dead = true;
+        for(int i=0; i<10; i++) {
+            enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
+        }
+        assertEquals(heroHP, hero.hitPoint);
     }
 }
